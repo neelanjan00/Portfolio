1cbfa97e0afca115a2912805def37f17cfc54035
Neelanjan Manna <neelanjanmanna@gmail.com>
2021-04-15 15:13:59 +0530

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,14 +4,7 @@ import "firebase/storage"
 import "firebase/auth"
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyC42MHSCGEaJFVCI0rpGLSu_-0nduFLvAA",
-    authDomain: "neelanjan-manna.firebaseapp.com",
-    databaseURL: "https://neelanjan-manna.firebaseio.com",
-    projectId: "neelanjan-manna",
-    storageBucket: "neelanjan-manna.appspot.com",
-    messagingSenderId: "853491017807",
-    appId: "1:853491017807:web:6d3ebdce91ac4f38dae23a",
-    measurementId: "G-0XDFFT78QF"
+    /*Replace with your own Firebase Project details*/
 })
 
 const db = firebaseApp.firestore()
