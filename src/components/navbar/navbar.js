0a121f493abad3100d865d101befbf0a5124026e
Neelanjan Manna <neelanjan.manna@harness.io>
2022-05-31 21:33:02 +0530

    added clickOutside hook for collapsing sidebar on click outside
    Signed-off-by: Neelanjan Manna <neelanjan.manna@harness.io>

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,10 +1,11 @@
 import { Link, useHistory, useLocation } from 'react-router-dom'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { getHomeIcon, getBlogIcon, getProjectIcon, getContactMeIcon, getLogoutIcon, getHamburgerIcon } from '../../assets/inline-svgs'
 import { auth } from '../../services/firebase'
 import MobileNavbarTile from '../mobile-navbar-tile/mobile-navbar-tile'
 
 import useWindowSize from '../../hooks/useWindow'
+import useOutsideClick from '../../hooks/useClickOutside'
 
 const Navbar = (props) => {
 
@@ -15,6 +16,16 @@ const Navbar = (props) => {
 
     const history = useHistory()
     const [width] = useWindowSize()
+
+    const sidebarRef = useRef();
+    const hamburgerIconRef = useRef();
+
+    const isClickedOutside = useOutsideClick(sidebarRef, hamburgerIconRef)
+
+    useEffect(() => {
+        if(isClickedOutside && displaySidebar)
+            setDisplaySidebar(false)
+    }, [isClickedOutside, displaySidebar])
 
     const logoutHandler = event => {
         event.preventDefault()
@@ -60,48 +71,48 @@ const Navbar = (props) => {
     if (width >= 1280) {
         return (
             <>
-            <div style={navbarStyle} className="pr-5 p-3">
-                <div style={{ float: 'right' }}>
-                    <Link to="/" style={{ textDecoration: 'none' }}>
-                        <span style={{ color: 'white' }}>
-                            <h5 style={{ display: 'inline' }}>Home</h5>
-                        </span>
-                    </Link>
-                    <Link to="/blog" style={{ textDecoration: 'none' }}>
-                        <span className="ml-5" style={{ color: 'white' }}>
-                            <h5 style={{ display: 'inline' }}>Blogs</h5>
-                        </span>
-                    </Link>
-                    <Link to="/projects" style={{ textDecoration: 'none' }}>
-                        <span className="ml-5" style={{ color: 'white' }}>
-                            <h5 style={{ display: 'inline' }}>Projects</h5>
-                        </span>
-                    </Link>
-                    <span className="ml-5" style={{ color: 'white', cursor: 'pointer' }}>
-                        <h5 style={{ display: 'inline' }} onClick={scrollToBottom}>Contact Me</h5>
-                    </span>
-                    {
-                        auth.currentUser === null 
-                            ? null 
-                            : <span className="ml-5" onClick={logoutHandler} style={{ color: 'white', cursor: 'pointer' }}>
-                                <h5 style={{ display: 'inline' }}>Logout</h5>
+                <div style={navbarStyle} className="pr-5 p-3">
+                    <div style={{ float: 'right' }}>
+                        <Link to="/" style={{ textDecoration: 'none' }}>
+                            <span style={{ color: 'white' }}>
+                                <h5 style={{ display: 'inline' }}>Home</h5>
                             </span>
-                    }
+                        </Link>
+                        <Link to="/blog" style={{ textDecoration: 'none' }}>
+                            <span className="ml-5" style={{ color: 'white' }}>
+                                <h5 style={{ display: 'inline' }}>Blogs</h5>
+                            </span>
+                        </Link>
+                        <Link to="/projects" style={{ textDecoration: 'none' }}>
+                            <span className="ml-5" style={{ color: 'white' }}>
+                                <h5 style={{ display: 'inline' }}>Projects</h5>
+                            </span>
+                        </Link>
+                        <span className="ml-5" style={{ color: 'white', cursor: 'pointer' }}>
+                            <h5 style={{ display: 'inline' }} onClick={scrollToBottom}>Contact Me</h5>
+                        </span>
+                        {
+                            auth.currentUser === null
+                                ? null
+                                : <span className="ml-5" onClick={logoutHandler} style={{ color: 'white', cursor: 'pointer' }}>
+                                    <h5 style={{ display: 'inline' }}>Logout</h5>
+                                </span>
+                        }
+                    </div>
                 </div>
-            </div>
-            {
-                location.pathname === '/' 
-                    ? null
-                    : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                        <path fill="#000000" fillOpacity="1" d="M0,224L720,160L1440,256L1440,0L720,0L0,0Z"></path>
-                    </svg>
-            }
+                {
+                    location.pathname === '/'
+                        ? null
+                        : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                            <path fill="#000000" fillOpacity="1" d="M0,224L720,160L1440,256L1440,0L720,0L0,0Z"></path>
+                        </svg>
+                }
             </>
         )
     } else {
         return (
             <React.Fragment>
-                <div className="p-2" style={{
+                <div className="p-2" ref={hamburgerIconRef} style={{
                     minWidth: '100vw', backgroundColor: 'black',
                     position: 'sticky', top: '0', zIndex: '3'
                 }} >
@@ -109,25 +120,23 @@ const Navbar = (props) => {
                         {getHamburgerIcon('white')}
                     </span>
                 </div>
+                <div ref={sidebarRef} style={{
+                    zIndex: '2', minHeight: '100vh', width: displaySidebar ? '250px' : '0',
+                    backgroundColor: 'black', position: 'fixed', transition: '0.3s'
+                }}>
+                    <MobileNavbarTile icon={getHomeIcon('white')} label="Home" highlightNavigation={location.pathname === '/'} route="/" displaySidebar={displaySidebar} />
+                    <MobileNavbarTile icon={getBlogIcon('white')} label="Blogs" highlightNavigation={location.pathname.includes('blog')} route="/blog" displaySidebar={displaySidebar} />
+                    <MobileNavbarTile icon={getProjectIcon('white')} label="Project" highlightNavigation={location.pathname === '/projects'} route="/projects" displaySidebar={displaySidebar} />
+                    <MobileNavbarTile icon={getContactMeIcon('white')} label="Contact Me" clickHandler={contactMeMobileView} displaySidebar={displaySidebar} />
 
-                { (
-                    <div style={{
-                        zIndex: '2', minHeight: '100vh', width: displaySidebar ? '250px' : '0',
-                        backgroundColor: 'black', position: 'fixed', transition: '0.3s'}}>
-                        <MobileNavbarTile icon={getHomeIcon('white')} label="Home" highlightNavigation={location.pathname === '/'} route="/" displaySidebar={displaySidebar} />
-                        <MobileNavbarTile icon={getBlogIcon('white')} label="Blogs" highlightNavigation={location.pathname.includes('blog')} route="/blog" displaySidebar={displaySidebar} />
-                        <MobileNavbarTile icon={getProjectIcon('white')} label="Project" highlightNavigation={location.pathname === '/projects'} route="/projects" displaySidebar={displaySidebar} />
-                        <MobileNavbarTile icon={getContactMeIcon('white')} label="Contact Me" clickHandler={contactMeMobileView} displaySidebar={displaySidebar} />
-                        
-                        {
-                            auth.currentUser === null 
-                                ? null 
-                                : <MobileNavbarTile icon={getLogoutIcon('white')} label="Logout" clickHandler={logoutHandler} displaySidebar={displaySidebar} />
-                        }
-                    </div>
-                )}
+                    {
+                        auth.currentUser === null
+                            ? null
+                            : <MobileNavbarTile icon={getLogoutIcon('white')} label="Logout" clickHandler={logoutHandler} displaySidebar={displaySidebar} />
+                    }
+                </div>
                 {
-                    location.pathname === '/' 
+                    location.pathname === '/'
                         ? null
                         : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                             <path fill="#000000" fillOpacity="1" d="M0,224L720,160L1440,256L1440,0L720,0L0,0Z"></path>
